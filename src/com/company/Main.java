64cb7fa0c84f9c203f90f6f577259cea798a5b7a
Anda <andapaegle96@gmail.com>
2021-02-25 18:05:16 +0200

    Added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,16 @@
 package com.company;
 
+import java.text.DecimalFormat;
 import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 
 
 public class Main {
+    public static DecimalFormat df = new DecimalFormat("0.00");
 
     public static void main(String[] args) {
         Assignment_1();
-        Assignment_2Stack();
+        Stack();
         Assignment_2Queue();
-        Assignment2();
         Assignment3HashTable();
         Assignment3();
     }
@@ -90,12 +89,13 @@ public class Main {
         return newlist;
     }
 
-    public static void Assignment_2Stack() {
+    public static void Stack() {
         Stack<String> stringsForPalidrome = new Stack<>();
         stringsForPalidrome.push("Mom");
         stringsForPalidrome.push("Was it a car or a cat I saw?");
         stringsForPalidrome.push(" Madam, in Eden, I’m Adam.");
         stringsForPalidrome.push(" Yo, banana boy!");
+
 
         System.out.printf("Are those strings a palindrome?" +
                         "Mom: %s   Was it a car or a cat I saw?: %s  " +
@@ -107,21 +107,28 @@ public class Main {
     }
 
     public static void Assignment_2Queue() {
-        BlockingQueue<String> queueforPalidrome = new ArrayBlockingQueue(4);
-        queueforPalidrome.add("Mom");
-        queueforPalidrome.add("Was it a car or a cat I saw?");
-        queueforPalidrome.add(" Madam, in Eden, I’m Adam.");
-        queueforPalidrome.add(" Yo, banana boy!");
+        System.out.print("Enter any string:");
+        Scanner in = new Scanner(System.in);
+        String string = in.nextLine();
+        Queue queue = new LinkedList();
 
-        for (String sentence : queueforPalidrome) {
-            System.out.println(sentence);
-            if (isPalindrome(sentence)) {
-                System.out.println("This sentence is a palindrome");
-            } else {
-                System.out.println("This sentence is not a palindrome");
-            }
+        String inputString = string.toLowerCase().replaceAll("[^a-z]*", "");
+        for (int i = inputString.length() - 1; i >= 0; i--) {
+            queue.add(inputString.charAt(i));
         }
+
+        String reverseString = "";
+
+        while (!queue.isEmpty()) {
+            reverseString = reverseString + queue.remove();
+        }
+        if (inputString.equals(reverseString))
+            System.out.println("The input String is a palindrome.");
+        else
+            System.out.println("The input String is not a palindrome.");
+
     }
+
 
     public static boolean isPalindrome(String text) {
         StringBuilder reverse = new StringBuilder();
@@ -133,57 +140,80 @@ public class Main {
         return (reverse.toString()).equals(clean);
     }
 
-    public static void Assignment2() {
-        Deque<String> deque = new ArrayDeque<>();
-        deque.add("Mom");
-        deque.add("Was it a car or a cat I saw?");
-        deque.add(" Madam, in Eden, I’m Adam.");
-        deque.add(" Yo, banana boy!");
-
-        for (String sentence : deque) {
-            System.out.println(sentence);
-            if (isPalindrome(sentence)) {
-                System.out.println("This sentence is a palindrome");
-            } else {
-                System.out.println("This sentence is not a palindrome");
+    public static int under1eur(Map<String, Double> map) {
+        int counter = 0;
+        for (double price : map.values()) {
+            if (price < 1) {
+                counter++;
             }
         }
+        return counter;
+    }
+
+    public static String priceLowest(Map<String, Double> map) {
+        String minProduct = null;
+        double minPrice = Double.MAX_VALUE;
+        for (String product : map.keySet()) {
+            double price = map.get(product);
+            if (price < minPrice) {
+                minProduct = product;
+                minPrice = price;
+            }
+        }
+        return minProduct;
+    }
+
+    public static String priceLargest(Map<String, Double> map) {
+        String maxProduct = null;
+        double maxPrice = Double.MIN_VALUE;
+        for (String product : map.keySet()) {
+            double price = map.get(product);
+            if (price > maxPrice) {
+                maxProduct = product;
+                maxPrice = price;
+            }
+        }
+        return maxProduct;
+    }
+
+    public static void sortedHashTable(Hashtable<String, Double> map) {
+        map.entrySet().stream()
+                .sorted((k1, k2) -> +k1.getValue().compareTo(k2.getValue()))
+                .forEach(k -> System.out.print(" " + df.format(k.getValue()) + " = " + k.getKey() + " "));
+    }
+
+    public static void sortedHashmap(HashMap<String, Double> map) {
+        map.entrySet().stream()
+                .sorted((k1, k2) -> +k1.getValue().compareTo(k2.getValue()))
+                .forEach(k -> System.out.print(" " + df.format(k.getValue()) + " = " + k.getKey() + " "));
     }
 
     public static void Assignment3() {
-        HashMap<Double, String> products = new HashMap<>();
-        products.put(3.09, "Peanut Butter");
-        products.put(0.73, "Bananas");
-        products.put(0.98, "Apples");
-        products.put(6.55, "Strawberries");
-        products.put(9.99, "Salmon");
-        products.put(4.39, "Olive oil");
-        products.put(1.57, "Almond milk");
-        products.put(1.49, "Eggs");
-        products.put(1.99, "Cheese");
+        HashMap<String, Double> products = new HashMap<>();
+        products.put("Peanut Butter", 3.89);
+        products.put("Bananas", 0.74);
+        products.put("Apples", 0.89);
+        products.put("Strawberries", 6.87);
+        products.put("Salmon", 9.99);
+        products.put("Olive oil", 4.39);
+        products.put("milk", 0.79);
+        products.put("Eggs", 1.49);
+        products.put("Cheese", 3.12);
 
-        Double minPrice = Collections.min(products.keySet());
-        System.out.println("Lowest price: " + products.get(minPrice) + " " + minPrice + " eur");
-        Double maxPrice = Collections.max(products.keySet());
-        System.out.println("Highest price: " + products.get(maxPrice) + " " + maxPrice + " eur");
+        System.out.format("%d products are under 1 EUR\n" +
+                        "%s price is the lowest and %s price is the largest",
+                under1eur(products), priceLowest(products), priceLargest(products));
 
-        int count = 0; // under 1 eur
-        for (Double price : products.keySet()) {
-            if (price < 1) {
-                count++;
-            }
-        }
-        System.out.println("How many products are under 1 eur? " + count);
 
-        HashMap<Double, String> moreProducts = new HashMap<>();
-        moreProducts.put(2.19, "Lentils");
-        moreProducts.put(1.35, "beans");
-        moreProducts.put(0.79, "Bread");
-        moreProducts.put(1.49, "Chocolate");
-        moreProducts.put(0.99, "Smoothie");
-        moreProducts.put(3.29, "Macadamia nuts");
-        moreProducts.put(1.89, "Butter");
-        moreProducts.put(0.89, "Napkins");
+        HashMap<String, Double> moreProducts = new HashMap<>();
+        moreProducts.put("Lentils", 2.19);
+        moreProducts.put("Peanut Butter", 5.34);
+        moreProducts.put("Bread", 0.87);
+        moreProducts.put("Chocolate", 2.35);
+        moreProducts.put("Smoothie", 1.46);
+        moreProducts.put("Macadamia nuts", 3.26);
+        moreProducts.put("Butter", 2.89);
+        moreProducts.put("Napkins", 0.50);
 
         products.putAll(moreProducts);
         System.out.println("Merged list:");
@@ -191,56 +221,51 @@ public class Main {
             System.out.println(list.getKey() + " " + list.getValue());
         }
         System.out.println("Products in the merged list: " + products.size());
-        System.out.println("Sorted in ascending order:");
-        Map<Double, String> sortedProducts = new TreeMap<>(products); //sort the merged HashMap
-        for (Map.Entry sort : sortedProducts.entrySet()) {
-            System.out.println(sort.getKey() + " " + sort.getValue());
-        }
+
+        HashMap<String, Double> productsMerged = new HashMap<>(products);
+        moreProducts.forEach((key, value) -> productsMerged.merge(key, value, (v1, v2) -> (v1 + v2) / 2));
+        System.out.println("Merged HashMap is: ");
+        productsMerged.forEach((key, value) -> System.out.print(" " + key + " = " + df.format(value)));
+        System.out.format("\nThere are %d products in merged HashMap \n", productsMerged.size());
+
+        System.out.println("Sorting ascending HashMap: ");
+        sortedHashmap(productsMerged);
     }
 
     public static void Assignment3HashTable() {
-        Hashtable<Double, String> products = new Hashtable<>();
-        products.put(3.09, "Peanut Butter");
-        products.put(0.73, "Bananas");
-        products.put(0.98, "Apples");
-        products.put(6.55, "Light bulb");
-        products.put(9.99, "Salmon");
-        products.put(4.39, "Olive oil");
-        products.put(1.57, "Almond milk");
-        products.put(1.49, "Eggs");
-        products.put(1.99, "Cheese");
+        Hashtable<String, Double> products = new Hashtable<>();
+        products.put("Peanut Butter", 3.89);
+        products.put("Bananas", 0.74);
+        products.put("Apples", 0.89);
+        products.put("Strawberries", 6.87);
+        products.put("Salmon", 9.99);
+        products.put("Olive oil", 4.39);
+        products.put("milk", 0.79);
+        products.put("Eggs", 1.49);
+        products.put("Cheese", 3.12);
 
-        Double minPrice = Collections.min(products.keySet());
-        System.out.println("Lowest price: " + products.get(minPrice) + " " + minPrice + " eur");
-        Double maxPrice = Collections.max(products.keySet());
-        System.out.println("Highest price: " + products.get(maxPrice) + " " + maxPrice + " eur");
+        System.out.format("Products are under 1 EUR %s \n" +
+                        "%s price is the lowest and %s price is the largest",
+                under1eur(products), priceLowest(products), priceLargest(products));
 
-        int count = 0;
-        for (Double price : products.keySet()) {
-            if (price < 1) {
-                count++;
-            }
-        }
-        System.out.println("How many products are under 1 eur? " + count);
+        Hashtable<String, Double> moreProducts = new Hashtable<>();
+        moreProducts.put("Lentils", 2.19);
+        moreProducts.put("Peanut Butter", 5.34);
+        moreProducts.put("Bread", 0.87);
+        moreProducts.put("Chocolate", 2.35);
+        moreProducts.put("Smoothie", 1.46);
+        moreProducts.put("Macadamia nuts", 3.26);
+        moreProducts.put("Butter", 2.89);
+        moreProducts.put("Napkins", 0.50);
 
-        Hashtable<Double, String> moreProducts = new Hashtable<>();
-        moreProducts.put(2.19, "Lentils");
-        moreProducts.put(1.35, "beans");
-        moreProducts.put(0.79, "Bread");
-        moreProducts.put(1.49, "Chocolate");
-        moreProducts.put(0.99, "Smoothie");
-        moreProducts.put(5.29, "Strawberry");
-        moreProducts.put(1.89, "Butter");
-        moreProducts.put(0.89, "Napkins");
+        Hashtable<String, Double> productsMerged = new Hashtable<>(products);
+        moreProducts.forEach((key, value) -> productsMerged.merge(key, value, (v1, v2) -> (v1 + v2) / 2));
+        System.out.println("Merged HashTable is: ");
+        productsMerged.forEach((key, value) -> System.out.print(" " + key + " = " + df.format(value)));
 
-        products.putAll(moreProducts);
-
-        System.out.println("How many products in the merged list? " + products.size());
+        System.out.println("How many products in the merged list? " + productsMerged.size());
         System.out.println("Sorted in ascending order:");
-        Map<Double, String> sortedProducts = new TreeMap<>(products); //sort the merged HashMap
-        for (Map.Entry sort : sortedProducts.entrySet()) {
-            System.out.println(sort.getKey() + " " + sort.getValue());
-        }
+        sortedHashTable(productsMerged);
 
     }
 }
